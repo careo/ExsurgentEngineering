35fb7712d93d62e8bf7a85b873bf682f44698c34
careo <careo@fastmail.fm>
2013-10-23 19:21:30 -0700

    Add gimbalRange config value for stock ModuleGimbal compatability.

--- ExsurgentEngineering/SmarterGimbal.cs
+++ ExsurgentEngineering/SmarterGimbal.cs
@@ -10,7 +10,10 @@ namespace ExsurgentEngineering
 		public float pitchRange = 10f;
 
 		[KSPField (isPersistant = false)]
-		public float yawRange = 0f;
+		public float yawRange;
+
+		[KSPField (isPersistant = false)]
+		public float gimbalRange;
 
 		[KSPField (isPersistant = false)]
 		public String gimbalTransformName;
@@ -33,8 +36,6 @@ namespace ExsurgentEngineering
 		[KSPEvent (guiName = "Free Gimbal", guiActive = true)]
 		public void FreeGimbal ()
 		{
-//			Debug.Log ("FreeGimbal()");
-
 			gimbalLock = false;
 			Events ["FreeGimbal"].active = false;
 			Events ["LockGimbal"].active = true;
@@ -43,8 +44,6 @@ namespace ExsurgentEngineering
 		[KSPEvent (guiName = "Lock Gimbal", guiActive = true)]
 		public void LockGimbal ()
 		{
-//			Debug.Log ("LockGimbal()");
-
 			gimbalLock = true;
 			Events ["FreeGimbal"].active = true;
 			Events ["LockGimbal"].active = false;
@@ -60,38 +59,31 @@ namespace ExsurgentEngineering
 		[KSPAction ("Toggle Gimbal")]
 		public void ToggleAction (KSPActionParam param)
 		{
-//			Debug.Log ("param: " + param);
-//			Debug.Log ("param.type: " + param.type);
-
 			if (param.type == KSPActionType.Activate)
 				FreeGimbal ();
 			else
 				LockGimbal ();
-
 		}
 
 		public override string GetInfo ()
 		{
-			return "Smarter Thrust Vectoring Enabled.";
+			return String.Format ("Smarter Thrust Vectoring Enabled\n - Pitch: {0}\n - Yaw: {1}", pitchRange, yawRange);
 		}
 
 		float RollAxis ()
 		{
 			if (part.rigidbody != null) {
-
 				var vesselCoM = vessel.findWorldCenterOfMass ();          
 				var partCoM = part.rigidbody.worldCenterOfMass;
 				var dot = Vector3.Dot (vesselCoM - partCoM, vessel.transform.right);
 				return dot;
 			}
 			return 0f;
-
 		}
 
 		float PitchAxis ()
 		{
 			if (part.rigidbody != null) {
-
 				var vesselCoM = vessel.findWorldCenterOfMass ();          
 				var partCoM = part.rigidbody.worldCenterOfMass;
 				var dot = Vector3.Dot (vesselCoM - partCoM, vessel.transform.up);
@@ -103,7 +95,6 @@ namespace ExsurgentEngineering
 		float YawAxis ()
 		{
 			if (part.rigidbody != null) {
-
 				var vesselCoM = vessel.findWorldCenterOfMass ();          
 				var partCoM = part.rigidbody.worldCenterOfMass;
 				// FIXME: now I'm just guessing at the axis to use
@@ -111,7 +102,16 @@ namespace ExsurgentEngineering
 				return dot;
 			}
 			return 0f;
+		}
 
+		public override void OnLoad (ConfigNode node)
+		{
+			// use gimbalRange for pitch and yaw ranges if present
+			if (node.HasValue ("gimbalRange")) {
+				var configRange = float.Parse (node.GetValue ("gimbalRange"));
+				pitchRange = configRange;
+				yawRange = configRange;
+			}
 		}
 
 		public override void OnStart (StartState state)
@@ -124,12 +124,11 @@ namespace ExsurgentEngineering
 			foreach (var gimbalTransform in part.FindModelTransforms(gimbalTransformName)) {
 				transformsAndRotations.Add (gimbalTransform, gimbalTransform.localRotation);
 			}
-
-
 		}
 
 		public override void OnFixedUpdate ()
 		{
+			// FIXME: really, what should the guard conditions be? 
 			if (HighLogic.LoadedSceneIsEditor)
 				return;
 			if (FlightGlobals.ActiveVessel != vessel)
@@ -143,8 +142,7 @@ namespace ExsurgentEngineering
 				var rollDotSign = Mathf.Sign (rollDot);
 
 				var roll = vessel.ctrlState.roll;
-				rollAngle = roll * rollDotSign * pitchRange * -1; // TODO: figure out why the -1 is needed. I didn't think it was'
-
+				rollAngle = roll * rollDotSign * pitchRange * -1; // TODO: figure out why the -1 is needed. I didn't think it was
 			}
 
 			var pitchDot = PitchAxis ();
@@ -161,7 +159,6 @@ namespace ExsurgentEngineering
 
 			var gimbalAngle = pitchAngle + rollAngle;
 			gimbalAngle = Mathf.Clamp (gimbalAngle, -pitchRange, pitchRange);
-
 
 			currentPitchAngle = gimbalAngle;
 			currentYawAngle = yawAngle;
@@ -183,12 +180,8 @@ namespace ExsurgentEngineering
 					gimbalTransform.localRotation = Quaternion.RotateTowards (gimbalTransform.localRotation, targetRotation, angle);
 				} else {
 					gimbalTransform.localRotation = targetRotation;
-
 				}
-
-
 			}
-
 		}
 	}
 }
